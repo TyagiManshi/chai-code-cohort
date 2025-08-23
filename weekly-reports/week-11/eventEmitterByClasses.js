a637153cbb630d1def6bd7a1eea9251febc0e275
Manshi Tyagi <manshityagi1919@gmail.com>
2025-08-23 21:14:26 +0530

    Update eventEmitterByClasses.js

--- weekly-reports/week-11/eventEmitterByClasses.js
+++ weekly-reports/week-11/eventEmitterByClasses.js
@@ -1,1 +1,20 @@
-hii
+const EventEmitter = require("events");
+
+class Chat extends EventEmitter{
+    sendMessage(msg){
+        console.log(`Message sent: ${msg}`);
+        this.emit('messageRecieved', msg);
+    }
+}
+
+const chat = new Chat();
+chat.on("messageRecieved", (msg) => {
+    console.log(`New Message: ${msg}`);
+    
+});
+
+
+// trigger event
+chat.sendMessage("Hello user");
+
+
